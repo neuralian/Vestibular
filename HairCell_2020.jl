5677c37ee70b8863e08b583249af1f46d6862c51
Mike Paulin <neuralian@gmail.com>
2020-02-20 17:50:14 +1300

    Depolarization panel working but ...
    ... problems with aspect ratio in MakieLayout sub-plots. Markers are squished. Do I have to write my own "scatter" using polys?

--- HairCell_2020.jl
+++ HairCell_2020.jl
@@ -18,6 +18,7 @@ z  = 40.e-15   # Gating force 40 fN (Howard, Roberts & Hudspeth 1988)
 d  = 3.5e-9    # Gate swing distance 3.5nm
 pᵣ = 0.15      # resting/spontaneous open state probability
 Nch = 48       # number of gating channels
+nm = 1e-9  # nanometers
 
 # simulation parameters
 
@@ -26,6 +27,24 @@ kcx0 = 600.   # location of kinocilium in scene (pixels from BL)
 kcy0 = 600.   # ""
 pRange = 1e-7  # range of probabilities to plot (pRange, 1-pRange)
 hairScale = 0.05 # scale deflection from plot to gate state animation
+maxTime = 1000   # duration of time series plots (haircell state and spikes)
+
+min_deflect = -500.0
+max_deflect = 1000.0
+deflect_range = collect(min_deflect:max_deflect)
+
+# time series plot time base
+t = collect(0:maxTime)
+
+
+# solve p₀(x₀)= 1/2 (deflection when open state prob = 1/2)
+x₀ =  kᵦ*T*log( (1-pᵣ)/pᵣ)/z
+
+# solve p₀(xRange)= pRange to find plot range
+xRange =  kᵦ*T*log( (1-pRange)/pRange)/z
+
+# open state probability as a function of bundle deflection
+p_open(x) = 1.0./(1.0 .+ exp.(-z*(x.-x₀)/(kᵦ*T)))
 
 CleanAxis(scene) = LAxis(scene,
                           titlevisible = false,
@@ -44,16 +63,30 @@ nRows = nPlots+1
 scene_layout = GridLayout(scene, nRows, 2,
                     colsizes = [Relative(0.3), Relative(0.7)],
                     rowsizes = [Relative(0.6), Relative(0.2), Relative(0.2)],
-                    alignmode = Outside(10, 10, 10, 10))
-# scene_layout[1,1] = control_panel = LRect(scene)
+                    alignmode = Outside(30, 30, 30, 30))
+
+
+# Hair cell animation pane
 scene_layout[1,2]    = hc_animation_axis = LAxis(scene)
-scene_layout[2, 1:2] = hc_potential_axis = CleanAxis(scene)
+hc_animation_axis.xlabel  = "Deflection /nm"
+hc_animation_axis.ylabel = "Open Probability"
+hc_animation_axis.xgridvisible = false
+hc_animation_axis.ygridvisible = false
+# plot open state probability as a function of kinocilium deflection
+lines!(hc_animation_axis, deflect_range,  p_open(deflect_range*nm),
+           linewidth =4,
+           color = :darkcyan,
+           leg = false,
+           limits = FRect(-500., -0.1, 1500., 1.2)
+      )
+# hc_animation_axis.limits[] = FRect(-500., -0.1, 1500., 1.2)
 
-scene_layout[3, 1:2] = afferent_spike_axis = CleanAxis(scene)
+# hair cell state (depolarization atm)
+scene_layout[2, 1:2] = hc_state_axis = CleanAxis(scene)
+hc_state = fill(0.0, maxTime+1)
+hc_state_plot = lines!(hc_state_axis, t, hc_state, color = :darkcyan)
+hc_state_axis.limits[] = FRect(0., -515., 1001., 1530.)
 
-# layout[1:3, 1] = axs = [LAxis(scene) for i in 1:3]
-
-scatter!(hc_potential_axis, rand(Point2f0, 20))
 
 
 
@@ -61,71 +94,30 @@ display(scene)
 
 
 
-# solve p₀(x₀)= 1/2 (deflection when open state prob = 1/2)
-x₀ =  kᵦ*T*log( (1-pᵣ)/pᵣ)/z
 
-# solve p₀(xRange)= pRange to find plot range
-xRange =  kᵦ*T*log( (1-pRange)/pRange)/z
+scene_layout[3, 1:2] = afferent_spike_axis = CleanAxis(scene)
 
-# open state probability as a function of bundle deflection
-p₀(x) = 1.0./(1.0 .+ exp.(-z*(x.-x₀)/(kᵦ*T)))
 
-# x-axis for animation pane
-nPts = 100.
-xScale = 1e-9    # x-axis in nm
-x = (x₀ .+ collect((-nPts/2.):(nPts/2.))/nPts*xRange)/xScale
+
+
+# # x-axis for animation pane
+# nPts = 100.
+# xScale = 1e-9    # x-axis in nm
+# x = (x₀ .+ collect((-nPts/2.):(nPts/2.))/nPts*lengthUnit)/xScale
 
 # slider to control kinocilium deflection
-kinocilium_slider  = LSlider(scene, range = LinRange(x[1], x[end], 100))
+kinocilium_slider  = LSlider(scene,
+                             range = LinRange(min_deflect, max_deflect, 100))
 scene_layout[1,1] = kinocilium_slider
 
 
- lines!(hc_animation_axis, x,  p₀(x*xScale),
-             linewidth =4,
-             color = :darkcyan,
-             leg = false
-        )
 
- hc_animation_axis.xlabel  = "Bundle deflection /nm"
- hc_animation_axis.ylabel = "Open State Probability"
- hc_animation_axis.xgridvisible = false
-  hc_animation_axis.ygridvisible = false
+display(scene)
 
-# deflectionPane = layout[2,1] #limits=FRect(0,-20., 1000,20.))
-# t = 1:1000
-# w = randn(size(t))*10.
-# plot!(deflectionPane, t, w  , scale_plot = false,
-#    show_axis = false)
-# D = deflectionPane[end]
-#
-# depolarizationPane =  Scene(limits=FRect(0,-5, 1000,5))
-# plot!(depolarizationPane,  randn(1000)*10., scale_plot = false,
-#    show_axis = false)
-#
-#
-#
-# # # maximum sensitivity Δp₀ per nm
-# # dx = 1e-9   # 1nm
-# # Smax = (p₀(x₀+dx)-p₀(x₀-dx))/(2.0*dx)  # = slope at x₀
-# # Λ2 = 1.0/(2.0*Smax) # half-space constant
-# # x1 = (x₀ .+ collect((-nPts):(nPts))/nPts*Λ2)/xScale
-# # lines!(x1,  .5 .+ Smax*(x1.-x₀/xScale)*xScale, color = :salmon)
-#
-# # # sensitivity at resting position
-# # Srest = (p₀(dx)-p₀(-dx))/(2.0*dx)  # = slope at x₀
-# # D = pᵣ/Srest
-# # x2 = collect((-nPts/3):(nPts))/(nPts/3)*D/xScale
-# # lines!(x2,  pᵣ .+ Srest*x2*xScale, color = :orange4)
-#
-# # # Shannon entropy of gate states
-# # Hmax = entropy(Binomial(Nch,.5), 2.0)
-# # H(p) = entropy(Binomial(Nch,p),2.0)/Hmax
-# # lines!(x, map(H,p₀(x*xScale)), color=:darkgoldenrod3)
-#
 function drawHairCell(panel, x0,y0, state)
 
   dx = 20.
-  dy = 14.
+  dy = 16.
 
   # kinocilium, drawn in scene at (x0, y0)
   scatter!(panel, [x0],[y0],
@@ -162,32 +154,34 @@ function drawHairCell(panel, x0,y0, state)
   c = [state[i] ? :gold1 : :dodgerblue1 for i in 1:48]
   scatter!(panel, x,y,
         marker=:circle,
-        markersize = 16,
         color = c,
+        markersize = 16,
         strokewidth = .5,
         strokecolor=:black)
 
 
-  panel[end]  # return handle to hair cell bundle
+  return panel[end]  # return handle to hair cell bundle
 
 end
 
 # draw hair cell (resting state)
 
-haircell_handle = drawHairCell(scene, kcx0, kcy0, rand(48).<pᵣ)
+haircell_handle = drawHairCell(scene,kcx0, kcy0, rand(48).<pᵣ)
 
 # kinocilium_deflection = kinocilium_slider[end][:value]
 # vbox(s1, parent=control_panel)
 
 # draw kinocillium deflection indicator
 scatter!(scene, [kcx0+kinocilium_slider.value[]*hairScale, kcy0+0.5],
-                [kcx0+0.5,kcy0+p₀(kinocilium_slider.value[]*xScale)],
-                marker = [:hexagon,:circle],
+                [kcx0+0.5,kcy0+p₀(kinocilium_slider.value[])],
+                marker = [:hexagon,:hexagon],
                 color = [RGB(.5,0.,.5), RGBA(.5,0.,.5,.25)],
-                markersize = [18, 18],
+                markersize = [18, 16],
                 strokewidth = 0.5,
                 strokecolor = :black)
 kinocilium_handle = scene[end]  # Array{Point{2,Float32},1} coordinates
+
+display(scene)
 #
 #
 #
@@ -208,19 +202,20 @@ kinocilium_handle = scene[end]  # Array{Point{2,Float32},1} coordinates
   # nb deflection is an Observable whose (observed) value is deflection[]
   # Similarly randn(1) is a 1-element array of random numbers
   #    and randn(1)[] (or randn(1)[1]) is a random number
-  Δk = kinocilium_slider.value[] +Float32(randn(1)[])*2.0
+  Δk = kinocilium_slider.value[] +2.0*randn(1)[]
 
-  p = p₀(Δk*xScale)
+
+
+  p = p_open(Δk*nm)
   gateState = rand(48).<p
   haircell_handle[:color] = [gateState[i] ? :gold1 : :dodgerblue1 for i in 1:48]
 
   kinocilium_handle[1][] = [Point2f0(kcx0+Δk*hairScale, kcy0+0.5),
                             Point2f0(kcx0+Δk,  kcy0+p)]
 
-  # dScale = .5
-  # push!(deleteat!(w,1), Δk*dScale)
-  # D[2] = w
-
+  dScale = .5
+  push!(deleteat!(hc_state,1), Δk)
+  hc_state_plot[2] = hc_state
   sleep(.005)
 
 
